27de713753273f47cb42b60770a93c1263eb32e7
Niks <58349866+kloyaa@users.noreply.github.com>
2024-04-22 14:17:00 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
+    <Center>
       <h1>Hi, I'm Kolya! I will finish my portfolio soon.</h1>
-    
-    </>
+    </Center>
   )
 }
 
